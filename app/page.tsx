7997f9397a3503b42131751a3bea71f3ccf15352
Ahmed-Armaan <ahmedarmaan2022@gmail.com>
2026-01-20 04:51:11 +0530

    Auto login redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <div>Welcome to getFoods...</div>
-    </div>
-  );
+	redirect("/login");
 }
